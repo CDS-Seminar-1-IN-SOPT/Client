5d6210ab24199a321b89a520d6441c17033235f5
1 <wrasf175@naver.com>
2022-11-12 17:52:28 +0900

    feat: 컬러스타일 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,3 +1,12 @@
 export const theme = {
-  colors: {},
+  colors: {
+    purple: '#9880F9',
+    pink: '#FF54B0',
+    red: '#FE3459',
+    grey_01: '#333333',
+    grey_02: '#33333399',
+    grey_03: '#33333366',
+    grey_04: '#33333333',
+    white: '#FFFFFF',
+  },
 };
