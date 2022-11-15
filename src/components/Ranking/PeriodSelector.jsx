e8170cc9ceaf2d83f869bedbc4bce224bc4726d2
1 <wrasf175@naver.com>
2022-11-16 02:47:50 +0900

    feat✨: 주간, 일간 선택 컴포넌트 추가

---
+++ src/components/Ranking/PeriodSelector.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function PeriodSelector() {
+  return (
+    <Styled.Root>
+      <h1>여긴 날짜 선택 컴포넌트입니다.</h1>
+    </Styled.Root>
+  );
+}
+
+export default PeriodSelector;
+
+const Styled = {
+  Root: styled.div``,
+};
