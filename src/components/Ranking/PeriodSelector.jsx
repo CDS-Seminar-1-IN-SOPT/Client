857f247c0d348e1a04cf9a6018967c86acc8c334
1 <wrasf175@naver.com>
2022-11-17 04:16:54 +0900

    style🎨: 기간 선택 컴포넌트 퍼블리싱

--- src/components/Ranking/PeriodSelector.jsx
+++ src/components/Ranking/PeriodSelector.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import { theme } from 'styles/theme';
+import { theme, mixins } from 'styles/theme';
 
 function PeriodSelector() {
   return (
     <Styled.Root>
-      <Styled.Box>일간/ 주간</Styled.Box>
+      <Styled.PeriodContainer>
+        <Styled.DayPeriod>일간</Styled.DayPeriod>
+        <Styled.WeekPeriod>주간</Styled.WeekPeriod>
+      </Styled.PeriodContainer>
     </Styled.Root>
   );
 }
@@ -17,5 +20,29 @@ const Styled = {
     font-size: ${theme.fontSizes.body01};
     font-weight: ${theme.fontWeights.body01};
   `,
-  Box: styled.div``,
+  PeriodContainer: styled.div`
+    ${mixins.rowFlexBox}
+    width: 34rem;
+    height: 4.1rem;
+    border-radius: 0.5rem;
+    margin-left: 1.2rem;
+  `,
+  DayPeriod: styled.div`
+    ${mixins.rowFlexBox}
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.grey_01};
+    border-radius: 0.5rem 0 0 0.5rem;
+    width: 50%;
+    height: 100%;
+  `,
+  WeekPeriod: styled.div`
+    ${mixins.rowFlexBox}
+    color: ${theme.colors.grey_02};
+    background-color: ${theme.colors.white};
+    border: 0.1rem solid ${theme.colors.grey_04};
+    border-left: none;
+    border-radius: 0 0.5rem 0.5rem 0;
+    width: 50%;
+    height: 100%;
+  `,
 };
