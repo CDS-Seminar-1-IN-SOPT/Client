195fa6d8d0b2066aefadd3755bf9ad4dda665dad
hayoun <fishingest@kookmin.ac.kr>
2022-11-18 03:14:38 +0900

    feat✨:Genre 이미지

--- src/components/Home/Content/GenreChoice.jsx
+++ src/components/Home/Content/GenreChoice.jsx
@@ -1,31 +1,48 @@
+import concert from 'assets/Icons/icn_concert.svg';
+import event from 'assets/Icons/icn_event.svg';
+import family from 'assets/Icons/icn_family.svg';
+import musical from 'assets/Icons/icn_musical.svg';
+import play from 'assets/Icons/icn_play.svg';
+import sing from 'assets/Icons/icn_sing.svg';
 import styled from 'styled-components';
-
 function GenreChoice() {
   return (
     <Styled.GenreChoice>
       <Styled.GenreDiv>
-        <Styled.GenreImg />
-        <Styled.GerneText />
+        <Styled.GenreImg src={concert} />
+        <Styled.GerneText>콘서트</Styled.GerneText>
       </Styled.GenreDiv>
       <Styled.GenreDiv>
-        <Styled.GenreImg />
-        <Styled.GerneText />
+        <Styled.GenreImg src={musical} />
+        <Styled.GerneText>뮤지컬</Styled.GerneText>
       </Styled.GenreDiv>
       <Styled.GenreDiv>
-        <Styled.GenreImg />
-        <Styled.GerneText />
+        <Styled.GenreImg src={play} />
+        <Styled.GerneText>연극</Styled.GerneText>
       </Styled.GenreDiv>
       <Styled.GenreDiv>
-        <Styled.GenreImg />
-        <Styled.GerneText />
+        <Styled.GenreImg src={sing} />
+        <Styled.GerneText>
+          무용&
+          <br />
+          클래식
+        </Styled.GerneText>
       </Styled.GenreDiv>
       <Styled.GenreDiv>
-        <Styled.GenreImg />
-        <Styled.GerneText />
+        <Styled.GenreImg src={event} />
+        <Styled.GerneText>
+          행사&
+          <br />
+          전시회
+        </Styled.GerneText>
       </Styled.GenreDiv>
       <Styled.GenreDiv>
-        <Styled.GenreImg />
-        <Styled.GerneText />
+        <Styled.GenreImg src={family} />
+        <Styled.GerneText>
+          가족&
+          <br />
+          어린이
+        </Styled.GerneText>
       </Styled.GenreDiv>
     </Styled.GenreChoice>
   );
@@ -35,7 +52,6 @@ const Styled = {
   GenreChoice: styled.section`
     width: auto;
     margin: 3.6rem 2.75rem 0px 2.75rem;
-    background-color: black;
     height: 30rem;
     display: flex;
   `,
