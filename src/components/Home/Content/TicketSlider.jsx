8938c085dfd8744ff876e79727791168d73981d7
hayoun <fishingest@kookmin.ac.kr>
2022-11-17 21:57:27 +0900

    feat✨:Swiper 기능 구현

--- src/components/Home/Content/TicketSlider.jsx
+++ src/components/Home/Content/TicketSlider.jsx
@@ -1,13 +1,66 @@
-import useAPI from 'cores/hooks/useAPI';
-import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import { EffectCoverflow, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 function TicketSlider({ bannerData }) {
   // console.log(bannerData);
-  return <Styled.TicketSlider>{bannerData[0].id}</Styled.TicketSlider>;
+
+  return (
+    <Styled.TicketSlider>
+      <SwiperStyle />
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={1.5}
+        spaceBetween={30}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 140,
+          modifier: 1.5,
+          slideShadows: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+          styled: Styled.SwiperPagination,
+        }}
+        modules={[EffectCoverflow, Pagination]}
+        navigation={true}
+        className="mySwiper">
+        {bannerData.map((v) => (
+          <Styled.Swiper key={v.url}>
+            <Styled.SwieperImg src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          </Styled.Swiper>
+        ))}
+      </Swiper>
+    </Styled.TicketSlider>
+  );
 }
 
 const Styled = {
-  TicketSlider: styled.section``,
+  TicketSlider: styled.section`
+    overflow: hidden;
+    height: 700px;
+  `,
+  Swiper: styled(SwiperSlide)`
+    width: 37.5rem;
+    overflow: visible;
+  `,
+  SwieperImg: styled.img`
+    height: 24.2rem;
+    width: 23.2rem;
+    border-radius: 1.5rem;
+  `,
+  SwiperPagination: styled.div`
+    position: relative;
+    top: 300px;
+  `,
 };
 
 export default TicketSlider;
