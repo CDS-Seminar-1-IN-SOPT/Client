c97d5eebae638e38fd6c69b0348ab54b125affc6
Dev-jwJeong <rgn95652@gmail.com>
2022-11-17 23:23:59 +0900

    style🎨: calendar header(prev,next,now Date) 컴포넌트

---
+++ src/components/DateOff/Calendar/CalendarHeader.jsx
@@ -0,0 +1,79 @@
+import prev from 'assets/Icons/icn_back_m.svg';
+import next from 'assets/Icons/icn_foward_m.svg';
+import { format } from 'date-fns';
+import React from 'react';
+import styled from 'styled-components';
+import { mixins, theme } from 'styles/theme';
+
+function CalendarHeader() {
+  return (
+    <Styled.Root>
+      <Styled.PrevButton>
+        <Styled.PrevImage src={prev} alt="prev" />
+      </Styled.PrevButton>
+      <Styled.NowDate>
+        <Styled.NowYear>{format(new Date(), 'M')}월</Styled.NowYear>
+        <Styled.NowMonth>
+          <Styled.NowMonth>{format(new Date(), 'yyyy')}</Styled.NowMonth>
+        </Styled.NowMonth>
+      </Styled.NowDate>
+      <Styled.NextButton>
+        <Styled.NextImage src={next} alt="next" />
+      </Styled.NextButton>
+    </Styled.Root>
+  );
+}
+
+export default CalendarHeader;
+
+const Styled = {
+  Root: styled.div`
+    width: 100%;
+    height: 8.3rem;
+    position: relative;
+  `,
+  NowDate: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    top: 2.3rem;
+    left: 17rem;
+  `,
+  NowYear: styled.span`
+    font-weight: ${theme.fontWeights.title01};
+    font-size: ${theme.fontSizes.title01};
+    color: ${theme.colors.grey_01};
+    height: 2.2rem;
+  `,
+  NowMonth: styled.span`
+    font-weight: ${theme.fontWeights.title03};
+    font-size: ${theme.fontSizes.body01};
+    color: ${theme.colors.grey_02};
+    height: 1.5rem;
+  `,
+  PrevButton: styled.button`
+    position: absolute;
+    width: 3.2rem;
+    height: 3.2rem;
+    top: 2.6rem;
+    left: 8.7rem;
+    ${mixins.rowFlexBox}
+    border-radius: 0.8rem;
+    border: 0.945914px solid ${theme.colors.grey_04};
+    background-color: ${theme.colors.white};
+  `,
+  PrevImage: styled.img``,
+  NextButton: styled.button`
+    position: absolute;
+    width: 3.2rem;
+    height: 3.2rem;
+    top: 2.6rem;
+    right: 8.8rem;
+    ${mixins.rowFlexBox}
+    border-radius: 0.8rem;
+    border: 0.945914px solid ${theme.colors.grey_04};
+    background-color: ${theme.colors.white};
+  `,
+  NextImage: styled.img``,
+};
