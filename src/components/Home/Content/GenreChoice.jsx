9c51df964bd0ac339a088700299a88f2296d0aa5
hayoun <fishingest@kookmin.ac.kr>
2022-11-17 22:17:18 +0900

    style🎨: GenreView margin 스타일링

--- src/components/Home/Content/GenreChoice.jsx
+++ src/components/Home/Content/GenreChoice.jsx
@@ -1,5 +1,15 @@
+import styled from 'styled-components';
+
 function GenreChoice() {
-  return <></>;
+  return <Styled.GenreChoice />;
 }
 
+const Styled = {
+  GenreChoice: styled.section`
+    width: auto;
+    margin: 3.6rem 2.75rem 0px 2.75rem;
+    background-color: black;
+    height: 3rem;
+  `,
+};
 export default GenreChoice;
