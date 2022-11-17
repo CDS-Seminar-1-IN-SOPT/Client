62e61ba5ecb525f31e7c042af7db2430e8d9329b
1 <wrasf175@naver.com>
2022-11-17 16:43:20 +0900

    style🎨: ranking page view Navbar 컴포넌트 정적 디자인  퍼블리싱

--- src/components/Ranking/Navbar.jsx
+++ src/components/Ranking/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import { theme } from 'styles/theme';
+import { theme, mixins } from 'styles/theme';
 
 function Navbar() {
   return (
     <Styled.Root>
-      <Styled.List>전체뮤지컬연극클래식어린이등등등</Styled.List>
+      <Styled.List>
+        <Styled.Item>전체</Styled.Item>
+        <Styled.Item>콘서트</Styled.Item>
+        <Styled.Item>뮤지컬</Styled.Item>
+        <Styled.Item>연극</Styled.Item>
+        <Styled.Item>클래식</Styled.Item>
+        <Styled.Item>전시회</Styled.Item>
+        <Styled.Item>어린이</Styled.Item>
+      </Styled.List>
     </Styled.Root>
   );
 }
@@ -16,6 +24,20 @@ const Styled = {
   Root: styled.div`
     font-size: ${theme.fontSizes.body02};
     font-weight: ${theme.fontWeights.body02};
+    position: absolute;
+    width: 34.8rem;
+    height: 3.5rem;
+    left: 1.3rem;
+    top: 9.4rem;
   `,
-  List: styled.ul``,
+  List: styled.ul`
+    ${mixins.rowFlexBox};
+    padding-right: 0.5rem;
+  `,
+  Item: styled.li`
+    ${mixins.rowFlexBox};
+    width: 100%;
+    height: 3.5rem;
+    border-bottom: 0.1rem solid ${theme.colors.grey_04};
+  `,
 };
