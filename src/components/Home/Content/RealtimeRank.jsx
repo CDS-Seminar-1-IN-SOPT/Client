b2126c437bb3fd735ed9cc42c8d59e86bd4daa84
hayoun <fishingest@kookmin.ac.kr>
2022-11-18 13:50:59 +0900

    feat✨:실시간 랭킹 컴포넌트

--- src/components/Home/Content/RealtimeRank.jsx
+++ src/components/Home/Content/RealtimeRank.jsx
@@ -1,5 +1,36 @@
+import styled from 'styled-components';
+
 function RealtimeRank() {
-  return <></>;
+  return (
+    <Styled.RealtimeRank>
+      <Styled.RealtimeInfo>
+        <Styled.RealtimeText>실간 티켓 랭킹</Styled.RealtimeText>
+        <Styled.RealtimeLink />
+      </Styled.RealtimeInfo>
+      <Styled.RankDiv>
+        <Styled.RealtimeImg />
+        <Styled.RealtimeRankBox>1</Styled.RealtimeRankBox>
+      </Styled.RankDiv>
+      <Styled.RankDiv>
+        <Styled.RealtimeImg />
+        <Styled.RealtimeRankBox>2</Styled.RealtimeRankBox>
+      </Styled.RankDiv>
+      <Styled.RankDiv>
+        <Styled.RealtimeImg />
+        <Styled.RealtimeRankBox>3</Styled.RealtimeRankBox>
+      </Styled.RankDiv>
+    </Styled.RealtimeRank>
+  );
 }
+
+const Styled = {
+  RealtimeRank: styled.section``,
+  RealtimeInfo: styled.div``,
+  RealtimeText: styled.span``,
+  RealtimeLink: styled.img``,
+  RankDiv: styled.div``,
+  RealtimeImg: styled.img``,
+  RealtimeRankBox: styled.div``,
+};
 
 export default RealtimeRank;
