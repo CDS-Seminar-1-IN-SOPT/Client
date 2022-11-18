9f5f917a75c418753019f0c42dd62265d58cdcf7
Dev-jwJeong <rgn95652@gmail.com>
2022-11-18 10:11:30 +0900

    Feat✨: 달력 prev,next 기능 추가

--- src/components/DateOff/Calendar/Calendar.jsx
+++ src/components/DateOff/Calendar/Calendar.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import { addMonths, subMonths } from 'date-fns';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import CalendarBody from './CalendarBody';
 import CalendarHeader from './CalendarHeader';
 
 function Calendar() {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const prevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+
+  const nextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
   return (
     <Styled.Root>
-      <CalendarHeader />
+      <CalendarHeader currentMonth={currentMonth} prevMonth={prevMonth} nextMonth={nextMonth} />
+
+      <CalendarBody currentMonth={currentMonth} />
     </Styled.Root>
   );
 }
@@ -17,5 +30,6 @@ const Styled = {
   Root: styled.div`
     width: 100%;
     height: 37.9rem;
+    position: relative;
   `,
 };
