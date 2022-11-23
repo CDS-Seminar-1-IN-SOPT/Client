279c1b39fa40136982407136c222a8488155de23
1 <wrasf175@naver.com>
2022-11-23 23:10:02 +0900

    style🎨: 헤더 스타일링 구현

--- src/components/Feed/Header.jsx
+++ src/components/Feed/Header.jsx
@@ -1,12 +1,52 @@
+import back from 'assets/Icons/icn_back_l.svg';
+import menu from 'assets/Icons/icn_menu.svg';
+import mypage from 'assets/Icons/icn_mypage.svg';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Header() {
-  return <Styled.Root>여긴 헤더에용</Styled.Root>;
+  const navigate = useNavigate();
+
+  const goToPrevPage = () => {
+    navigate(`/ranking`);
+  };
+
+  return (
+    <Styled.Root>
+      <Styled.BackButton src={back} onClick={goToPrevPage} />
+      <Styled.MyPage src={mypage} />
+      <Styled.HamburgerIcon src={menu} />
+    </Styled.Root>
+  );
 }
 
 export default Header;
 
 const Styled = {
-  Root: styled.div``,
+  Root: styled.div`
+    height: 4rem;
+  `,
+  BackButton: styled.img`
+    cursor: pointer;
+    position: absolute;
+    width: 2.4rem;
+    height: 2.4rem;
+    left: 1.6rem;
+    top: 0.8rem;
+  `,
+  MyPage: styled.img`
+    position: absolute;
+    width: 2.4rem;
+    height: 2.4rem;
+    left: 30.3rem;
+    top: 0.8rem;
+  `,
+  HamburgerIcon: styled.img`
+    position: absolute;
+    width: 2.4rem;
+    height: 2.4rem;
+    left: 33.1rem;
+    top: 0.8rem;
+  `,
 };
